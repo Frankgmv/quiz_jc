d4452829fc5a50851bb36a05563deac526f72b3c
@FrankoGMv <giovanyvelasquez2004@gmail.com>
2022-10-26 11:01:15 -0500

    Inicio organización de preguntas

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+import {Preguntas} from "./preguntas.js";
+
+console.log("La pregunta 1 es: ",Preguntas[1].Pregunta);
---
+++ assets/js/preguntas.js
@@ -0,0 +1,8 @@
+
+
+export const Preguntas = [
+    { Id:1, Pregunta:'¿Cuánto es 1 por 1?', primeraR:'Lógicamente 1', SegundaR:'ps 2'},
+    { Id:2, Pregunta:'¿Cuánto es 2 por 3?', primeraR:'Lógicamente 6', SegundaR:'ps 2'},
+    { Id:3, Pregunta:'¿Cuánto es 3 por 4?', primeraR:'Lógicamente 6', SegundaR:'ps 12'},
+    { Id:4, Pregunta:'¿Cuánto es 4 por 2?', primeraR:'Lógicamente 8', SegundaR:'ps 9'},
+]
