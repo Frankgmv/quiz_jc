55aca14f4c3900669451490b0f2538077eb95019
@FrankoGMv <giovanyvelasquez2004@gmail.com>
2022-10-28 11:58:59 -0500

    Actualización preguntas

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,8 +1,23 @@
 
 
 export const Preguntas = [
-    { Id:1, Pregunta:'¿Cuánto es 1 por 1?', primeraR:'Lógicamente 1', SegundaR:'ps 2'},
-    { Id:2, Pregunta:'¿Cuánto es 2 por 3?', primeraR:'Lógicamente 6', SegundaR:'ps 2'},
-    { Id:3, Pregunta:'¿Cuánto es 3 por 4?', primeraR:'Lógicamente 6', SegundaR:'ps 12'},
-    { Id:4, Pregunta:'¿Cuánto es 4 por 2?', primeraR:'Lógicamente 8', SegundaR:'ps 9'},
+    { Id:1, Pregunta:'¿Cuál es el plato típico paisa?', primeraR:'Bandeja paisa', SegundaR:'Arroz paisa'},
+    { Id:2, Pregunta:'¿Cuál es la capital paisa?', primeraR:'Medellín', SegundaR:'Antioquia'},
+    { Id:3, Pregunta:'¿Cuál es el principal evento de agosto para los paisas?', primeraR:'la feria de flores', SegundaR:'Desfile de carrozas'},
+    { Id:4, Pregunta:'¿Cuál es el mito más importante de Antioquia?', primeraR:'El Sombrerón', SegundaR:'La Mandinga'},
+    { Id:5, Pregunta:'¿Cuál es el artista paisa más reconocido mundialmente?', primeraR:'Doris Salcedo', SegundaR:'Fernando Botero'},
+    { Id:6, Pregunta:'¿Cuál el edificio más alto de la capital?', primeraR:'El Coltejer', SegundaR:'Torre Colpatria'},
+    { Id:8, Pregunta:'¿Cuál es la batalla musical típica del paisa?', primeraR:'La trova', SegundaR:'El vallenato'},
+    { Id:7, Pregunta:'¿Quien es al arriero?', primeraR:'El que cobra la plata', SegundaR:'El que arrea las mulas'},
+    { Id:9, Pregunta:'¿Cómo usa el arriero encima?', primeraR:'Poncho, sombrero y zurriago', SegundaR:'Sobrero y dulceabrigo y machete'},
+    { Id:10, Pregunta:'¿Cuál es el Clásico del futbol?', primeraR:'Nacional vs Medellín', SegundaR:'Medellín vs Rio Negro'},
+    { Id:11, Pregunta:'¿Sistema de transporte más importante de Medellín?', primeraR:'Línea metro', SegundaR:'Copetran SAS'},
+    { Id:12, Pregunta:'Nombre del aeropuerto principal de los paisas?', primeraR:'Olaya Herrera', SegundaR:'José María Córdova'},
+    { Id:13, Pregunta:'Plato típico de diciembre para los paisas?', primeraR:'Buñuelo y natilla', SegundaR:'Sancocho y asado'},
+    { Id:14, Pregunta:'¿Qué es un perico para los paisa?', primeraR:'Un tinto', SegundaR:'café con leche'},
+    { Id:15, Pregunta:'¿?', primeraR:'', SegundaR:''},
+    { Id:16, Pregunta:'¿?', primeraR:'', SegundaR:''},
+    { Id:17, Pregunta:'¿?', primeraR:'', SegundaR:''},
+
+
 ]
