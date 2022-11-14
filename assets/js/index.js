445bdc34386f25a21eddd9c0697ed2309f4ec457
@FrankoGMv <giovanyvelasquez2004@gmail.com>
2022-11-13 23:55:40 -0500

    Agregando el evento enter al index

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,18 +2,32 @@
 const Boton = document.querySelector('#trigger_button');
 const Nombre = document.querySelector('#nombre_usuario');
 
+
+
+let NombreUser = Nombre.value;
+let correctas = [0];
+var cont = [0];
+var puntaje = [0];
+localStorage.setItem("NombreUsuario", NombreUser);
+localStorage.setItem("correctas", correctas);
+localStorage.setItem("cont", cont);
+localStorage.setItem("puntaje", puntaje);
+
+
+Nombre.addEventListener('keypress', (e) => {
+    let codeLetter = e.keyCode;
+
+    if (codeLetter == 13 && Nombre.value.length > 0) {
+        window.location = "assets/views/preguntas.html";
+    }
+});
+
 Boton.addEventListener('click', () => {
     if (Nombre.value.length == 0) {
         Nombre.focus();
     } else {
-        let NombreUser = Nombre.value;
-        let correctas = [0];
-        localStorage.setItem("NombreUsuario", NombreUser);
-        localStorage.setItem("correctas", correctas);
-        var cont = [0];
-        var puntaje = [0];
-        localStorage.setItem("cont", cont);
-        localStorage.setItem("puntaje", puntaje);
         window.location = "assets/views/preguntas.html";
     }
 });
+
+
