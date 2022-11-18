41223a96d07584651f1585c1fd020957826dbe4c
@FrankoGMv <giovanyvelasquez2004@gmail.com>
2022-11-18 09:44:16 -0500

    organizar preguntas

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -2,118 +2,22 @@ window.addEventListener('load', startAll);
 let init = JSON.parse(localStorage.getItem('cont'));
 
 const Preguntas = [
-    {
-        Pregunta: '¿Cuál es el plato típico paisa?',
-        'respuesta_1': 'Rellena',
-        'respuesta_2': 'Bandeja',
-        "respuesta_3": 'Frijoles',
-        'correcta': 2
-    },
-    {
-        Pregunta: '¿Cuál es la capital paisa?',
-        'respuesta_1': 'Medellín',
-        "respuesta_2": 'Antioquia',
-        'respuesta_3': 'Rio Negro',
-        'correcta': 1
-    },
-    {
-        Pregunta: '¿Cuál es el principal evento de agosto para los paisas?',
-        'respuesta_1': 'La alborada',
-        "respuesta_2": 'Desfile de carrozas',
-        'respuesta_3': 'la feria de flores',
-        'correcta': 3
-    },
-    {
-        Pregunta: '¿Cuál es un mito Antioqueño?',
-        'respuesta_1': 'La Llorona',
-        "respuesta_2": 'El Sombrerón',
-        'respuesta_3': 'La PataSola',
-        'correcta': 3
-    },
-    {
-        Pregunta: '¿Cuál es el artista paisa más reconocido mundialmente por su arte?',
-        'respuesta_1': 'Doris Salcedo',
-        "respuesta_2": 'Fernando Botero',
-        'respuesta_3': 'Belisario Betancur',
-        'correcta': 2
-    },
-    {
-        Pregunta: '¿Cuál es el edificio más alto de la capital paisa?',
-        "respuesta_1": 'Torre del Café',
-        'respuesta_2': 'Centro Coltejer',
-        'respuesta_3': 'Cámara de Comercio',
-        'correcta': 2
-    },
-    {
-        Pregunta: '¿Cuál es la batalla musical típica del paisa?',
-        'respuesta_1': 'El Joropo',
-        "respuesta_2": 'El Vallenato',
-        'respuesta_3': 'La Trova',
-        'correcta': 3
-    },
-    {
-        Pregunta: '¿Quién era Cosiaca?',
-        'respuesta_1': 'El creador de la cultura paisa',
-        "respuesta_2": 'El creador del himno del paisa',
-        'respuesta_3': 'El creador de muchos dichos paisas',
-        'correcta': 3
-    },
-    {
-        Pregunta: '¿Qué hace el arriero?',
-        'respuesta_1': 'El que trabajar con chivas',
-        "respuesta_2": 'El que trabaja con mulas',
-        'respuesta_3': 'EL que trabajar hormigas',
-        'correcta': 2
-    },
-    {
-        Pregunta: '¿Cuál es el clásico del futbol paisa?',
-        "respuesta_1": 'Medellín vs Rio Negro',
-        'respuesta_2': 'Medellín vs Santa fé',
-        'respuesta_3': 'Nacional vs Medellín',
-        'correcta': 3
-    },
-    {
-        Pregunta: 'Sistema de transporte más importante de Medellín',
-        "respuesta_1": 'Línea Copetran',
-        'respuesta_2': 'Línea Circular',
-        'respuesta_3': 'Línea Metro',
-        'correcta': 3
-    },
-    {
-        Pregunta: 'seleccione el nombre del aeropuerto principal de los paisas',
-        'respuesta_1': 'Olaya Herrera',
-        "respuesta_2": 'José María Córdova',
-        'respuesta_3': 'Alberto Jaramillo Sánchez',
-        'correcta': 2
-    },
-    {
-        Pregunta: '¿Cuál es el Plato típico de diciembre para los paisas?',
-        "respuesta_1": 'Asado y lechona',
-        'respuesta_2': 'Buñuelo y natilla',
-        'respuesta_3': 'Buñuelo y lechona',
-        'correcta': 2
-    },
-    {
-        Pregunta: '¿Qué es un perico para el paisa?',
-        'respuesta_1': 'Un polvo',
-        "respuesta_2": 'Café con leche',
-        'respuesta_3': 'Un loro',
-        'correcta': 2
-    },
-    {
-        Pregunta: 'Universidad pública de la capital',
-        'respuesta_1': 'Uniminuto',
-        "respuesta_2": 'UdeM',
-        'respuesta_3': 'UdeA',
-        'correcta': 3
-    },
-    {
-        Pregunta: '¿Cuál es el dulce típico de los ancianos?',
-        "respuesta_1": 'Los caramelos',
-        'respuesta_2': 'Obleas',
-        'respuesta_3': 'Las solteritas',
-        'correcta': 3
-    }
+    { Pregunta: '¿Cuál es el plato típico paisa?', 'respuesta_1': 'Rellena', 'respuesta_2': 'Bandeja', "respuesta_3": 'Frijoles', 'correcta': 2 },
+    { Pregunta: '¿Cuál es la capital paisa?', 'respuesta_1': 'Medellín', "respuesta_2": 'Antioquia', 'respuesta_3': 'Rio Negro', 'correcta': 1 },
+    { Pregunta: '¿Cuál es el principal evento de agosto para los paisas?', 'respuesta_1': 'La alborada', "respuesta_2": 'Desfile de carrozas', 'respuesta_3': 'la feria de flores', 'correcta': 3 },
+    { Pregunta: '¿Cuál es un mito Antioqueño?', 'respuesta_1': 'La Llorona', "respuesta_2": 'El Sombrerón', 'respuesta_3': 'La PataSola', 'correcta': 3 },
+    { Pregunta: '¿Cuál es el artista paisa más reconocido mundialmente por su arte?', "respuesta_1": 'Fernando Botero', 'respuesta_2': 'Doris Salcedo', 'respuesta_3': 'Belisario Betancur', 'correcta': 1 },
+    { Pregunta: '¿Cuál es el edificio más alto de la capital paisa?', "respuesta_1": 'Torre del Café', 'respuesta_2': 'Centro Coltejer', 'respuesta_3': 'Cámara de Comercio', 'correcta': 2 },
+    { Pregunta: '¿Cuál es la batalla musical típica del paisa?', 'respuesta_1': 'La Trova', "respuesta_2": 'El Vallenato', 'respuesta_3': 'El Joropo', 'correcta': 1 },
+    { Pregunta: '¿Quién era Cosiaca?', 'respuesta_1': 'El creador de la cultura paisa', "respuesta_2": 'El creador del himno del paisa', 'respuesta_3': 'El creador de muchos dichos paisas', 'correcta': 3 },
+    { Pregunta: '¿Qué hace el arriero?', 'respuesta_1': 'El que trabajar con chivas', "respuesta_2": 'El que trabaja con mulas', 'respuesta_3': 'EL que trabajar hormigas', 'correcta': 2 },
+    { Pregunta: '¿Cuál es el clásico del futbol paisa?', "respuesta_1": 'Medellín vs Rio Negro', 'respuesta_2': 'Medellín vs Santa fé', 'respuesta_3': 'Nacional vs Medellín', 'correcta': 3 },
+    { Pregunta: 'Sistema de transporte más importante de Medellín', 'respuesta_1': 'Línea Metro', "respuesta_2": 'Línea Copetran', 'respuesta_3': 'Línea Circular', 'correcta': 1 },
+    { Pregunta: 'seleccione el nombre del aeropuerto principal de los paisas', 'respuesta_1': 'Olaya Herrera', "respuesta_2": 'José María Córdova', 'respuesta_3': 'Alberto Jaramillo Sánchez', 'correcta': 2 },
+    { Pregunta: '¿Cuál es el Plato típico de diciembre para los paisas?', "respuesta_1": 'Asado y lechona', 'respuesta_2': 'Buñuelo y natilla', 'respuesta_3': 'Buñuelo y lechona', 'correcta': 2 },
+    { Pregunta: '¿Qué es un perico para el paisa?', 'respuesta_1': 'Un polvo', "respuesta_2": 'Café con leche', 'respuesta_3': 'Un loro', 'correcta': 2 },
+    { Pregunta: 'Universidad pública de la capital', 'respuesta_1': 'Uniminuto', "respuesta_2": 'UdeM', 'respuesta_3': 'UdeA', 'correcta': 3 },
+    { Pregunta: '¿Cuál es el dulce típico de los ancianos?', 'respuesta_1': 'Las solteritas', "respuesta_2": 'Los caramelos', 'respuesta_3': 'Obleas', 'correcta': 1 }
 ]
 
 let total_preguntas = [Preguntas.length];
@@ -140,6 +44,7 @@ progressBar.appendChild(bar);
 
 cambiar_pregunta(init);
 
+window.addEventListener('load', SizeDisplay);
 function startAll(e) {
     e.preventDefault();
     cont_pregunta1.addEventListener('click', agregar_clase1);
@@ -148,8 +53,16 @@ function startAll(e) {
     boton.addEventListener('click', Evaluar_respuesta);
     body.addEventListener('keypress', enter_pass)
 
+
 }
 
+function SizeDisplay (){
+    let ancho = document.documentElement.clientWidth;
+    console.log(ancho);
+
+    
+
+}
 
 function enter_pass(e) {
     e.preventDefault();
@@ -189,7 +102,7 @@ function cambiar_pregunta(cont) {
             if (e.keyCode == 13) {
                 setTimeout(() => {
                     location.href = "../Views/puntaje.html";
-                }, 850);
+                }, 750);
             }
         });
     }
@@ -261,7 +174,7 @@ function agregar_clase3(e) {
 }
 
 
-esCorrecta = (n_ask, id_preg) => {
+function esCorrecta (n_ask, id_preg)  {
     let answer;
     if (n_ask == Preguntas[id_preg].correcta) {
         answer = true;
